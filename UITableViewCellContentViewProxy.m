3e32357cafd036de284a32ab8823c270442711de
sstern <sstern@72bb13d0-f000-403e-beb8-1553beae081e>
2010-11-08 16:29:31 +0000

    handle monkeyID's for table cells
    git-svn-id: https://svn.gorillalogic.com/svn/fonemonkey4/trunk@9 72bb13d0-f000-403e-beb8-1553beae081e

---
+++ UITableViewCellContentViewProxy.m
@@ -0,0 +1,14 @@
+//
+//  UITableViewCellContentView.m
+//  FoneMonkey
+//
+//  Created by Stuart Stern on 11/3/10.
+//  Copyright 2010 Gorilla Logic, Inc. All rights reserved.
+//
+
+#import "UITableViewCellContentViewProxy.h"
+
+
+@implementation UITableViewCellContentViewProxy
+
+@end
